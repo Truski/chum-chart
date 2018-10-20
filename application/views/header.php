565ac6f7ffdef72ad70c5ef5ed3f6e6b8dd09fad
Dan Wojtowicz <dwojtowi@hawk.iit.edu>
2018-10-20 10:04:01 -0400

    Implement initial landing page

---
+++ application/views/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>ChumChart | Make an Alignment Chart with Your Friends!</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no" />
+    <link rel="stylesheet" href="/assets/css/screen.css" />
+  </head>
+  <body>
+    <h1><img src="/assets/images/logo.png" />ChumChart</h1>
+    <p>Make a D&amp;D Alignment Chart with your Friends</p>
