65f2fa14ff225b76606e71a152060a97e1e92b76
Dan Wojtowicz <dwojtowi@hawk.iit.edu>
2018-10-20 19:10:10 -0400

    Fix model syntax errors

--- application/models/ChumBase.php
+++ application/models/ChumBase.php
@@ -4,7 +4,7 @@ class Chumbase extends CI_Model {
 
 	public function __construct()
 	{
-		parent::construct();
+		parent::__construct();
 		$this->load->database();
 	}
 
@@ -26,27 +26,27 @@ class Chumbase extends CI_Model {
 		Get a row from Chart table representing a completed alignment
 		Returns an object representing a row
 	*/
-	public function getChart(chartID){
+	public function getChart($chartID){
 		return $this->db->query('SELECT * FROM Chart WHERE chartid=chartID');
 	}
 
 	/*
 		
 	*/
-	public function getUserCount(chartID){
+	public function getUserCount($chartID){
 		return $this->db->query('SELECT usrcount FROM Chart WHERE chartid=chartID');
 	}
 	/*
 		getSurveys
 		returns array of objects where each object is a row in Survey table
 	*/
-	public function getSurveys(chartID){
+	public function getSurveys($chartID){
 		return $this->db->query('SELECT * FROM Survey WHERE charid = chartID');
 	}
 	/*
 	 * yikes
 	 */
-	public function fillChart(chartID, lg, ln, lc, ng, tn, ne, cg, cn, ce){
+	public function fillChart($chartID, $lg, $ln, $lc, $ng, $tn, $ne, $cg, $cn, $ce){
 		$this->db->query('UPDATE Chart SET lg=lg, ln=ln, lc=lc, ng=ng, tn=tn, ne=ne, cg=cg, cn=cn, ce=ce, WHERE chartid=chartID');  
 	}	
 
