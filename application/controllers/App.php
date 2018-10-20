c3312b99a5fe5f1d0c318961d5c4bbfc27c45cb4
David Purdum <purdum41@gmail.com>
2018-10-20 15:43:56 -0400

    Add some databasy things

--- application/controllers/App.php
+++ application/controllers/App.php
@@ -17,4 +17,46 @@ class App extends CI_Controller {
 		$this->load->view('footer');
 	}
 
+	public function submitData($json) {
+		$obj = json_decode($json);
+
+		$name = $obj->name;
+		$email = $obj->email;
+		$urlid = $obj->urlid;
+
+		$dataObj = new dataToScore($obj->answers);
+		$eScore = $dataObj->eScore;
+		$mScore = $dataObj->mScore;
+
+		// Send survey data to database
+		InsertServey($urlid, $mScore, $eScore, $name, $email);
+
+		// Ask server for number of users in urlid
+		$numSurveys = getUserCount($urlid);
+
+		// If there's 9 surveys, create the alignment chart!
+		if ($numServeys == 9) {
+			createChart($urlid);
+		}
+		
+	}
+
+	private function createChart($urlid) {
+
+		// Get all surveys with corresponding urlid
+
+		
+
+	}
+
+	public function getChart($json) {
+		$obj = json_decode($json);
+
+		$urlid = $obj->urlid;
+
+		// Get urlid from table in database
+
+	}
+
 }
+?>
