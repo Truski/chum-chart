e80c5d19f43b2130151de59d8e21cfd7df604eb2
David Purdum <purdum41@gmail.com>
2018-10-20 22:20:06 -0400

    Updated with css colors

--- application/views/table.php
+++ application/views/table.php
@@ -6,7 +6,7 @@
 
     <?php for($j = 0; $j < 3; $j++): ?>
 
-        <div class="flexkiddo" id=<?=$i*3+j?> >
+        <div class="flexkiddo" id="<?=$i*3+$j?>"" >
 
         </div>
 
@@ -35,4 +35,13 @@
 
 }
 
+#0 { background-color: #FFFFFF}
+#1 { background-color: #6C9ACC}
+#2 { background-color: #3421CA}
+#3 { background-color: #FF9494}
+#4 { }
+#5 { background-color: #471087}
+#6 { background-color: #E80000}
+#7 { background-color: #780001}
+#8 { background-color: #000000}
 </style>
