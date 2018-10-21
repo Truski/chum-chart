e18e90b3d15667d498a1e4f479910e3af0d9a96b
Dan Wojtowicz <dwojtowi@hawk.iit.edu>
2018-10-21 09:23:31 -0400

    Fix mobile first; make it desktop first

--- application/views/header.php
+++ application/views/header.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no" />
     <link rel="stylesheet" href="/assets/css/screen.css" />
   </head>
-  <body class=".bg">
+  <body class="bg">
     <div id="container">
       <h1><img id="logo" src="/assets/images/logo.png" /><span style="position:relative; top: 7px;">Chum</span><span style="display: inline-block; transform: rotate(-15deg);position:relative; bottom: 5px;">Chart</span></h1>
       <p>Make a D&amp;D Alignment Chart with your Friends</p>
--- application/views/home.php
+++ application/views/home.php
@@ -1,12 +1,1 @@
-<style>
-body, html {
-    height: 100%;
-    margin: 0;
-}
-
-.bg {
-    /* The image used */
-    background-image: url("/assets/images/background.jpg");
-}
-</style>
 <button>Create a Chart</button>
